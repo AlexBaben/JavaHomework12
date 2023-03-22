d658eda25c4a26ebfe1a000e1674ca18feca7643
Alex Babenko <wertuniul@gmail.com>
2023-03-22 04:19:32 +0300

    JavaHomework12

---
+++ src/main/java/ru/netology/Epic.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import java.util.Arrays;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+    @Override
+    public boolean matches(String query) {
+        if (Arrays.asList(subtasks).contains(query)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netology/TasksTest.java
@@ -0,0 +1,50 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+    @Test
+    public void shouldFindInMeetingProject() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Приложение НетоБанка");
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldFindInMeetingTopic() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Выкатка 3й версии приложения");
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldFindInSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches("Позвонить родителям");
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldFindInEpic() {
+        Epic epic = new Epic(55, new String[]{"Проснуться", "Потянуться"});
+
+        boolean expected = true;
+        boolean actual = epic.matches("Проснуться");
+        Assertions.assertEquals(expected,actual);
+    }
+}
